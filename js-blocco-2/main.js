e9782c200d241284a3a273163b1b44da0c0510ca
Evanflow89 <ivanbaragone@gmail.com>
2022-05-16 12:40:40 +0200

    commit

---
+++ js-blocco-2/main.js
@@ -0,0 +1,58 @@
+// Crea 10 oggetti che rappresentano una zucchina.
+// Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+// Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+
+const zucchine = [
+    {
+        nome: "zucchina uno",
+        peso: 300,
+        lunghezza: 13,
+    },
+    {
+        nome: "zucchina due",
+        peso: 395,
+        lunghezza: 15.5,
+    },
+    {
+        nome: "zucchina tre",
+        peso: 454,
+        lunghezza: 18.7,
+    },
+    {
+        nome: "zucchina quattro",
+        peso: 236,
+        lunghezza: 9.7,
+    },
+    {
+        nome: "zucchina cinque",
+        peso: 144,
+        lunghezza: 8.6,
+    },
+    {
+        nome: "zucchina sei",
+        peso: 388,
+        lunghezza: 15.4,
+    },
+    {
+        nome: "zucchina sette",
+        peso: 409,
+        lunghezza: 17.3,
+    },
+    {
+        nome: "zucchina otto",
+        peso: 367,
+        lunghezza: 10.5
+    },
+    {
+        nome: "zucchina nove",
+        peso: 189,
+        lunghezza: 8.7,
+    },
+    {
+        nome: "zucchina dieci",
+        peso: 398,
+        lunghezza: 9.6,
+    },
+];
+
