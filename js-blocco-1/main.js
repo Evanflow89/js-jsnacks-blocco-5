f2413ee1ec79710725f08c65d86283efcb2377bd
Evanflow89 <ivanbaragone@gmail.com>
2022-05-16 12:29:12 +0200

    commit

---
+++ js-blocco-1/main.js
@@ -0,0 +1,57 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
+
+const zucchine = [
+    {
+        nome: "zucchina uno",
+        peso: 300,
+        lunghezza: 13,
+    },
+    {
+        nome: "zucchina due",
+        peso: 335,
+        lunghezza: 13.5,
+    },
+    {
+        nome: "zucchina tre",
+        peso: 454,
+        lunghezza: 15.7,
+    },
+    {
+        nome: "zucchina quattro",
+        peso: 236,
+        lunghezza: 9.7,
+    },
+    {
+        nome: "zucchina cinque",
+        peso: 144,
+        lunghezza: 8.6,
+    },
+    {
+        nome: "zucchina sei",
+        peso: 388,
+        lunghezza: 12.4,
+    },
+    {
+        nome: "zucchina sette",
+        peso: 409,
+        lunghezza: 15.3,
+    },
+    {
+        nome: "zucchina otto",
+        peso: 367,
+        lunghezza: 10.5
+    },
+    {
+        nome: "zucchina nove",
+        peso: 189,
+        lunghezza: 8.7,
+    },
+    {
+        nome: "zucchina dieci",
+        peso: 398,
+        lunghezza: 9.6,
+    },
+];
+
